c7664d18f60501b8303728cad9067230ae2c3395
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-02-23 00:37:55 -0500

    Add files via upload

---
+++ DinnerParty.java
@@ -0,0 +1,6 @@
+class DinnerParty extends Party{
+    private int dinnerChoice; //declares dinnerChoice variable
+    public int getDinnerChoice(){ return dinnerChoice;} // get method for dinnerChoice
+    public void setDinnerChoice(int dinnerChoice){ this.dinnerChoice = dinnerChoice;} //set method for dinnerChoice
+
+}
---
+++ UseDinnerParty.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UseDinnerParty{
+    public static void main(String[] args){
+        DinnerParty aDinnerParty = new DinnerParty(); // declares instance of DinnerParty class
+
+        Scanner sc = new Scanner(System.in); // adds scanner
+
+        System.out.println("Enter Number of guests for the party:");
+        aDinnerParty.setNoOfGuests(sc.nextInt()); //sets noOfGuests off what user wants
+        System.out.println("Enter the menu option: 1 for beef or 2 for lamb.");
+        aDinnerParty.setDinnerChoice(sc.nextInt()); //sets dinnerChoice off what user wants
+
+        System.out.println("The party has " + aDinnerParty.getNoOfGuests() + " guests"); //displays amount of guests using get method and prints
+        System.out.println("Menu option " + aDinnerParty.getDinnerChoice() + " will be served"); //displays dinner choice using get method and prints
+        aDinnerParty.displayInvitation(); //displays invitation
+    }
+}

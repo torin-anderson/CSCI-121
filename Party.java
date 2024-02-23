323211d54c3871d3fc5d7243a28b8468ff49432f
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-02-23 00:18:36 -0500

    Delete Party.java

--- Party.java
+++
@@ -1,8 +0,0 @@
-public class Party {
-    int noOfGuests;
-    public int getNoOfGuests(){ return noOfGuests;}
-    public void setNoOfGuests(int noOfGuests){this.noOfGuests = noOfGuests;}
-    void displayInvitation(){
-        System.out.println("Please come to my party!");
-    }
-}

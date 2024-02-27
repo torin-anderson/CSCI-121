0fa79bc8bd882567656087bec099cef9614270b1
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-02-26 21:10:48 -0500

    Delete DinnerParty.java

--- DinnerParty.java
+++
@@ -1,6 +0,0 @@
-class DinnerParty extends Party{
-    private int dinnerChoice; //declares dinnerChoice variable
-    public int getDinnerChoice(){ return dinnerChoice;} // get method for dinnerChoice
-    public void setDinnerChoice(int dinnerChoice){ this.dinnerChoice = dinnerChoice;} //set method for dinnerChoice
-
-}
